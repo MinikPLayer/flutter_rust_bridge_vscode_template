c0fb1c0c30174459ddae7e4965616b61ec56bdb5
M. Tomecki <39161575+MinikPLayer@users.noreply.github.com>
2023-06-30 10:59:31 +0200

    Update launch_native.bat

--- scripts/launch_native.bat
+++ scripts/launch_native.bat
@@ -1,4 +1,5 @@
 @echo off
 
-start /W scripts\\build_native.bat Debug
+start /W scripts\\build_native.bat Debug Debug
+set RUST_BACKTRACE="1",
 start /W build\\windows\\runner\\Debug\\flutter_rust_bridge_template.exe
